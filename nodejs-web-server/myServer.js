36fcb2542669cbf8d577f45772d11f81f4c15b44
sakku_116 <88846365+sakku116@users.noreply.github.com>
2022-02-18 17:40:40 +0700

    update latihan web service

--- nodejs-web-server/myServer.js
+++ nodejs-web-server/myServer.js
@@ -13,36 +13,39 @@ function requestListener(request, response) {
     yang biasa digunakan pada command 'curl' */
 
     /* MEMBUAT REQUEST HANDLER
-    secara default ketika kita menggunakan
+    secara default, ketika kita menggunakan
     response handler akan meresponse sebagai GET */
     const method = request.method;
     if(method === "GET") {
-        response.end('<h2><h1>Welcome to my Web Server!</h1></h2>')
-    } else if(method === "POST") {
+        response.end('<h1>Welcome to my Web Server!</h1>')
+    }
+    else if(method === "POST") {
         response.end('<h2>method yang dimasukkan: POST</h2>')
-    } else if(method === "PUT") {
+    }
+    else if(method === "PUT") {
         response.end('<h2>method yang dimasukkan: PUT</h2>')
-    } else if(method === "DELETE") {
+    }
+    else if(method === "DELETE") {
         response.end('<h2>method yang dimasukkan: DELETE</h2>')
     }
 
 }
 
-// .createServer akan me-pass 2 argumen 'request' dan 'response'
+/* .createServer() akan me-pass 2 argumen 'request' dan 'response' */
 const server = http.createServer(requestListener)
 
 const port = 5000;
 const host = 'localhost';
 
-/* listen mempunyai 4 parameter yaiut:
+function myListener(port, host) {
+    /* fungsi hanya sebagai callback */
+    console.log(`server berjalan pada http://${host}:${port}`)
+}
+
+/* listen mempunyai 4 parameter yaitu:
 - port
 - hostname
 - backlog
 - listeningListener
 tetapi secara default akan berisi port, host, listeningListener */
-
-function myListener(port, host) {
-    console.log(`server berjalan pada http://${host}:${port}`)
-}
-
 server.listen(port, host, myListener(port, host))
