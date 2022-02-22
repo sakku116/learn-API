f65e6847d1c45b09814ad1da365a96a42669cd2c
sakku_116 <88846365+sakku116@users.noreply.github.com>
2022-02-22 15:44:57 +0700

    update nodejs-web-server

--- nodejs-web-server/myServer.js
+++ nodejs-web-server/myServer.js
@@ -1,35 +1,63 @@
 console.log('membuat web server')
-
 const http = require('http');
 
 function requestListener(request, response) {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
 
-    /* MEMBUAT RESPONSE DEFAULTt */
+    /* MEMBUAT RESPONSE DEFAULT */
     //response.end('<h1>Welcome to my Web Server!</h1>');
 
-    /* request berisi method seperti GET POST PUT DELETE
-    yang biasa digunakan pada command 'curl' */
-
     /* MEMBUAT REQUEST HANDLER
-    secara default, ketika kita menggunakan
-    response handler akan meresponse sebagai GET */
+    request biasanya berisi method seperti GET POST PUT DELETE
+    yang biasa digunakan pada command 'curl'.
+    secara default, response handler akan meresponse sebagai GET */
+    /* buat variable untuk memanggil method */
     const method = request.method;
-    
+    /* atau bisa menggunakan
+    const { method } = request;
+    untuk langsung mendapatkan si methodnya */
+
     if(method === "GET") {
         response.end('<h1>Welcome to my Web Server!</h1>')
-    }
-    else if(method === "POST") {
-        response.end('<h2>method yang dimasukkan: POST</h2>')
     }
     else if(method === "PUT") {
         response.end('<h2>method yang dimasukkan: PUT</h2>')
     }
+    else if(method === "POST") {
+        // latihan mendapatkan body request
+
+        let body = [];
+
+        function isiArray(chunk){
+            /* chunk akan berisi data yang dibawa callback dari fungsi event 'data'
+            ini akan menampung buffer */
+            body.push(chunk)
+            }
+        function convertBuffer(){
+            // mengubah buffer menjadi string json utuh
+            body = Buffer.concat(body).toString();
+
+            /* untuk menguraikan json.
+            panggil key dengan brackets untuk mendapatkan valuenya */
+            const { name } = JSON.parse(body);
+
+            // dan akhirnya buat response.end()
+            response.end(`<h1>Hai, ${name}!</h1>`);
+            }
+
+        // buat event 'data' dan 'end'
+        request.on('data', isiArray)
+        request.on('end', convertBuffer)
+
+        /*
+        coba untuk melakukan curl:
+        curl -X POST -H "Content-Type: application/json" http://localhost:5000 -d "{\"name\": \"Dicoding\"}"
+        */
+    }
     else if(method === "DELETE") {
         response.end('<h2>method yang dimasukkan: DELETE</h2>')
     }
-
 }
 
 /* .createServer() akan me-pass 2 argumen 'request' dan 'response' */
@@ -38,8 +66,7 @@ const server = http.createServer(requestListener)
 const port = 5000;
 const host = 'localhost';
 
-function myListener(port, host) {
-    /* fungsi hanya sebagai callback */
+function listeningListener(port, host) {
     console.log(`server berjalan pada http://${host}:${port}`)
 }
 
@@ -49,4 +76,4 @@ function myListener(port, host) {
 - backlog
 - listeningListener
 tetapi secara default akan berisi port, host, listeningListener */
-server.listen(port, host, myListener(port, host))
+server.listen(port, host, listeningListener(port, host))
